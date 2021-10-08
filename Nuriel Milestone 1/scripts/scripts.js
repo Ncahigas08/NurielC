104a26bb581de075c4c6b9654275766b887177d5
kahootgas <loomer14344@gmail.com>
2021-10-08 11:31:20 -0700

    commit

--- Nuriel Milestone 1/scripts/scripts.js
+++ Nuriel Milestone 1/scripts/scripts.js
@@ -1,10 +1,10 @@
-$(function () {
+$(function() {
     var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = 'views/' + $(this).data('include') + '.html'
-      $(this).load(file)
+    $.each(includes, function() {
+        var file = 'views/' + $(this).data('include') + '.html'
+        $(this).load(file)
     })
 
-    document.title = "your site name"
+    document.title = "OT SENIOR CAPSTONE CAHIGAS"
 
-  })
+})
